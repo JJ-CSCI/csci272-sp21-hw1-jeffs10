9714294a9b9e930a8104669f6bb5b3d110c24d35
jeffs10 <anthony.jeff911@gmail.com>
2021-02-25 01:44:38 +0000

    binomial.cpp and .h

--- binomial.h
+++ binomial.h
@@ -2,17 +2,29 @@
 #define BINOMIAL
 
 class Binomial {
+
     double coefft1, coefft2;
+
     int power1, power2;
-    public:
-    Binomial (double c1 = 1.0, int p1 = 1, double c2 = 1.0, int p2 = 1);
-    double GetCoefficient (int idx) const;
-    int GetPower(int idx) const;
+
+    public: Binomial (double c1 = 1.0, int p1 = 1, double c2 = 1.0, int p2 = 1);
+  
+    
+
     int SetPower (int idx, int pwr);
-    int Add (const Binomial other);
-    void Multiply (double f);
-    void Multiply (double coefft, int pwr);
-};
 
-#endif
+      int GetPower(int idx) const;
 
+        int Add (const Binomial other);
+   
+
+        void Multiply (double coefft, int pwr);
+
+          void Multiply (double f);
+
+
+   double GetCoefficient (int idx) const;
+
+    
+}
+;
